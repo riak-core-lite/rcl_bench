45b0747802adf46bcbdfe96688041fc4354a6101
Mariano Guerra <mariano@marianoguerra.org>
2020-09-26 19:36:09 +0200

    R scripts receive configs via command line arguments

--- R/throughput.R
+++ R/throughput.R
@@ -4,8 +4,12 @@ library(ggplot2)
 library(scales)
 library(lubridate)
 
-title <- "put"
-d <- read.csv(paste("../current/",title,"_single.csv", sep=""))
+args <- commandArgs(trailingOnly = TRUE)
+
+op_name <- args[1]
+csv_path <- args[2]
+img_path <- args[3]
+d <- read.csv(csv_path)
 
 d$timestamp <- d$timestamp / 1000
 d$timestamp <- as.POSIXct(d$timestamp, origin="1970-01-01")
@@ -17,5 +21,5 @@ d$tc <- as.POSIXct(d$tc, format="%Y-%m-%d %H:%M:%S")
 ggplot(d, aes(x=tc, y=n)) + 
 	geom_point(size=1)  +
 	geom_smooth() +
-	labs(title=paste(title, "throughput"), x = "Time", y = "Operations per second")
-ggsave(paste(title, "throughput.png", sep="_")) 
+	labs(title=paste(op_name, "throughput"), x = "Time", y = "Operations per second")
+ggsave(img_path) 
